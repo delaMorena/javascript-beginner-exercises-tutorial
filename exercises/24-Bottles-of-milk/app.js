704f1f17540e8e76c04b032c4d6b9029d96bbc78
delaMorena <martadmdo@gmail.com>
2020-10-05 13:04:04 +0000

    he avanzado algo en las botellas ej 24

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,12 +1,16 @@
-// Your code here:
+let bottles = 10;
 
-funcion sing() {
+function sing() {
     
-    let canción = [];
-    do {
-        console.log (i + "bottles of milk on the wall");
+      
+    for (bottles < 10; bottles >0; bottles--) {
+        console.log(bottles + " bottles of milk on the walk, " + bottles + " bottles of milk. Take one down and pass it around, "+ (bottles - 1) + " bottles of milk on the wall.");
     }
-    while (var i <= 100 && i > 2);
-
+    for (bottles < 2; bottles >=1; bottles--) {
+        console.log(bottles + " bottle of milk on the walk, " + bottles + " bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.");
+        console.log("No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.")
+    }
+    
 }
+
 sing();
